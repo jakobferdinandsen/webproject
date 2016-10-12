eba9ffa62784f2cdbc71e45f79cefda71a558d2b
Jakob Ferdinandsen <jakob@jhaaf.dk>
2016-10-12 12:37:51 +0200

    til morten

---
+++ js/playJS.js
@@ -0,0 +1,16 @@
+
+hello = "Hello world!";
+x = 10;
+console.log(hello);
+document.getElementById("myP").innerHTML = "Hello world!"
+
+console.log(true || false);
+
+var a = [1,1,1,1,1];
+
+function sayHi() {
+    name = document.getElementById("myInput").value;
+    console.log(name);
+}
+
+console.log(a)
