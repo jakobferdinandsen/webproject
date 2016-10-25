7d8d89eb5e996bba68cbdc841526721701496e5a
Jakob Ferdinandsen <jakob@jhaaf.dk>
2016-10-25 21:28:38 +0200

    Added dynamic input creation for rooms and added functionality to change room temp through the input

--- js/my.js
+++ js/my.js
@@ -1,36 +1,40 @@
-rooms = $(".room");
-
-for (i = 0; i < rooms.length; i++) {
-    console.log(rooms[i].style.height)
-    temp = rooms[i].innerHTML.match(/\d+\d+/)[0];
-    console.log()
-    if (temp > 25) {
-        rooms[i].style.backgroundColor = 'rgba(255, 0, 0, .7)';
-    }
-    if (temp <= 25 && temp >= 18) {
-        rooms[i].style.backgroundColor = 'rgba(0, 255, 0, .7)';
-    }
-    if (temp < 18) {
-        rooms[i].style.backgroundColor = 'rgba(0, 0, 255, .5)';
-    }
-}
-
-
 $(document).ready(function () {
     $('.room').each(function () {
         doInput($(this));
+        updateRoom($(this), $(this).html().match(/\d+\d+/)[0]);
+    })
+
+    $('.inputButton').click(function () {
+        var input = $(this).closest("tr").children().children()[0];
+        input.placeholder = "Temp "+input.value+"\xB0C";
+        roomId = input.id.slice(0, input.id.length - 6);
+        room = $('#'+roomId);
+        updateRoom(room, input.value);
     })
 })
 
 function doInput(room) {
-    inputTemplate = '<div><input id=' + room.attr('id') + 'input" type="number" placeholder="' + room.text() + '"><button>Change</button></div>';
-    inputContainer = $("#inputContainer");
-    inputContainer.append(inputTemplate);
-    inputContainer.children('div').each(function () {
-            $(this).click(function () {
-                $(this).fadeOut('slow').fadeIn('slow');
-            })
-        }
-    );
-    console.log()
+    inputTemplate =
+        '<tr>' +
+            '<td>' +
+                '<input id=' + room.attr('id') + 'input" type="number" placeholder="' + room.text() + '">' +
+            '</td>' +
+            '<td>' +
+                '<button class="inputButton">Change</button>' +
+            '</td>' +
+        '</tr>';
+    $("#inputContainer").append(inputTemplate);
+}
+
+function updateRoom(room, temp) {
+    room.html("Temp "+temp+"\xB0C")
+    if (temp > 25) {
+        room.css('backgroundColor', 'rgba(255, 0, 0, .7)');
+    }
+    if (temp <= 25 && temp >= 18) {
+        room.css('backgroundColor', 'rgba(0, 255, 0, .7)');
+    }
+    if (temp < 18) {
+        room.css('backgroundColor', 'rgba(0, 0, 255, .5)');
+    }
 }
